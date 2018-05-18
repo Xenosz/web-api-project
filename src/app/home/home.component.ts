c7aab62a4dfc87ea342b0e5b60d0603b8ed4eddf
brent <brent.swaenen@hotmail.com>
2018-05-18 12:25:12 +0200

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { MarvelService  } from "../services/marvel.service";
 
 @Component({
     selector: 'app-home',
@@ -6,4 +7,100 @@ import { Component } from "@angular/core";
 })
 export class HomeComponent
 {
+    characters:Result[];
+    character:Result;
+
+    constructor(private _svc: MarvelService){}
+
+
+
+}
+
+
+interface RootObject {
+    code: number;
+    status: string;
+    copyright: string;
+    attributionText: string;
+    attributionHTML: string;
+    etag: string;
+    data: Data;
+}
+interface Data {
+    offset: number;
+    limit: number;
+    total: number;
+    count: number;
+    results: Result[];
+}
+
+interface Result {
+    id: number;
+    name: string;
+    description: string;
+    modified: Date;
+    thumbnail: Thumbnail;
+    resourceURI: string;
+    comics: Comics;
+    series: Series;
+    stories: Stories;
+    events: Events;
+    urls: Url[];
+}
+
+interface Url {
+    type: string;
+    url: string;
+}
+
+interface Thumbnail {
+    path: string;
+    extension: string;
+}
+
+interface Item {
+    resourceURI: string;
+    name: string;
+}
+interface Item2 {
+    resourceURI: string;
+    name: string;
+}
+interface Item3 {
+    resourceURI: string;
+    name: string;
+    type: string;
+}
+interface Item4 {
+    resourceURI: string;
+    name: string;
+}
+
+
+interface Comics {
+    available: number;
+    collectionURI: string;
+    items: Item[];
+    returned: number;
+}
+
+interface Series {
+    available: number;
+    collectionURI: string;
+    items: Item2[];
+    returned: number;
+}
+
+interface Stories {
+    available: number;
+    collectionURI: string;
+    items: Item3[];
+    returned: number;
+}
+
+interface Events {
+    available: number;
+    collectionURI: string;
+    items: Item4[];
+    returned: number;
 }
---
+++ src/app/services/marvel.service.ts
@@ -0,0 +1,128 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import {Md5} from 'ts-md5/dist/md5';
+
+@Injectable()
+export class MarvelService {
+    apikey = "b26ebc1cda7557cffd998bcdb92e29d5";
+    privateKey = "eb39270df3985571052a3756726d826c33f7c065";
+    constructor(private _http: HttpClient) { }
+
+
+    GetHash(): string{
+        var timeStamp = Date.now()/1000
+        var prehash : string 
+        prehash = timeStamp.toString() + this.privateKey + this.apikey
+        return Md5.hashStr(prehash).toString()
+    }
+
+    GetCharacterNameBeginsWith(name:string,limit,offset): Observable<RootObject>{
+        name = "t";
+        limit = 10;
+        offset = 0;
+        return this._http.get<RootObject>("https://gateway.marvel.com:443/v1/public/characters?nameStartsWith="+ name + '&apikey=' + this.apikey + '&hash=' + this.GetHash() +'&ts='+ Date.now()/1000 +"&limit="+limit+"&offset="+offset);
+        //.do(data => { console.log(JSON.stringify(data)) }); //Test om te zien of de data wordt opgehaald
+    }
+
+    GetCharacterWithId(id): Observable<RootObject>{
+        return this._http.get<RootObject>("https://gateway.marvel.com:443/v1/public/characters/"+ id + '?apikey=' + this.apikey + '&hash=' + this.GetHash() +'&ts='+ Date.now()/1000);
+    }
+
+
+
+}
+
+
+
+
+        export interface RootObject {
+            code: number;
+            status: string;
+            copyright: string;
+            attributionText: string;
+            attributionHTML: string;
+            etag: string;
+            data: Data;
+        }
+        export interface Data {
+            offset: number;
+            limit: number;
+            total: number;
+            count: number;
+            results: Result[];
+        }
+
+        export interface Result {
+            id: number;
+            name: string;
+            description: string;
+            modified: Date;
+            thumbnail: Thumbnail;
+            resourceURI: string;
+            comics: Comics;
+            series: Series;
+            stories: Stories;
+            events: Events;
+            urls: Url[];
+        }
+
+        export interface Url {
+            type: string;
+            url: string;
+        }
+
+        export interface Thumbnail {
+            path: string;
+            extension: string;
+        }
+    
+        export interface Item {
+            resourceURI: string;
+            name: string;
+        }
+        export interface Item2 {
+            resourceURI: string;
+            name: string;
+        }
+        export interface Item3 {
+            resourceURI: string;
+            name: string;
+            type: string;
+        }
+        export interface Item4 {
+            resourceURI: string;
+            name: string;
+        }
+    
+    
+        export interface Comics {
+            available: number;
+            collectionURI: string;
+            items: Item[];
+            returned: number;
+        }
+    
+        export interface Series {
+            available: number;
+            collectionURI: string;
+            items: Item2[];
+            returned: number;
+        }
+    
+        export interface Stories {
+            available: number;
+            collectionURI: string;
+            items: Item3[];
+            returned: number;
+        }
+    
+        export interface Events {
+            available: number;
+            collectionURI: string;
+            items: Item4[];
+            returned: number;
+        }
+    
+    
+    
